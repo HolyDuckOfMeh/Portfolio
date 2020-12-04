857542c76c5bee9ebac0daf7f33b1c139921033c
HolyDuckOfMeh <zhengjason68@gmail.com>
2020-12-04 00:05:50 -0500

    navbar and imgs

---
+++ View/navbar.php
@@ -0,0 +1,13 @@
+<!-- Navbar -->
+    <div class='navbar-grid'>
+        <div class='navbar-img'>
+            <img style='height: 40px; width 40px;' src='./View/Public/Images/FOXHOUND.jpg'>
+        </div>
+        <div class='navbar-name'>
+            <h2>HolyDuckOfMeh</h2> 
+        </div>
+            <a href="" class='navbar-home-btn btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Home</a> 
+            <a href="" class='navbar-projects-btn btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Projects</a> 
+            <a href="" class='navbar-contact-btn btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Contact Me</a> 
+    </div>
+<!-- Navbar -->
