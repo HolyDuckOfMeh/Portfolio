8f73e1c144d7219654970659fa6b62e3d69aad24
Jason Zheng <jzheng@training.local>
2020-12-04 16:28:20 -0500

    navbar inprovements and image struggles

--- home.php
+++ home.php
@@ -4,3 +4,11 @@
     include "./View/navbar.php";
 
 ?>
+<div class='home-grid'>
+    <div class="home-bg-image"></div>
+
+        <div class="home-bg-text">
+            <h1>Hi, I'm Jason Zheng</h1>
+            <p>I am a</p>
+        </div>
+</div>
